74d4469c88d7b9989ec83037109673e037c0d568
Ivan Mercedes <ivanmercede@gmail.com>
2020-09-23 03:21:51 +0200

    agregando consulta a db

---
+++ produnthunternext/components/layout/DetalleProducto.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const DetalleProducto = ({producto}) => {
+
+    const {id, comentarios, creado, descripcion, empresa, nombre, url, urlimagen, votos} = producto;
+
+
+    return ( 
+      <li>
+          <div>
+            <div>
+
+            </div>
+            <div>
+                <h1>{nombre}</h1>
+            </div>
+          </div>
+
+          <div>
+
+          </div>
+      </li>
+     );
+}
+ 
+export default DetalleProducto;
--- produnthunternext/pages/index.js
+++ produnthunternext/pages/index.js
@@ -1,12 +1,49 @@
+import React, { useEffect, useState, useContext } from 'react';
+import DetalleProducto from '../components/layout/DetalleProducto';
 import Layout from '../components/layout/layout';
+import { FirebaseContext } from '../firebase';
 
-export default function Home() {
-  return (
-    <div>
-      <Layout>
-         <h1>hola mundo</h1>
+
+
+const Home = () => {
+
+  const [productos, guardarProducto] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+   const obtenerProductos = () =>{
+      firebase.db.collection('productos').orderBy('creado','desc').onSnapshot(menejarSnapshot)
+   }
+   obtenerProductos();
+
+   
+  }, []);
+
+  function menejarSnapshot(snapshot){
+    const productos = snapshot.docs.map(doc=>{
+      return {
+       id: doc.id,
+       ...doc.data()
+      }
+    });
+    guardarProducto(productos);
+   }
+  return ( 
+       <Layout>
+         <div className="listado-productos">
+           <div className="contenedor">
+              <ul className="bg-white">
+                {productos.map(producto => (
+                  <DetalleProducto 
+                  key={producto.id}
+                  producto={producto}/>
+                ))}
+              </ul>
+           </div>
+         </div>
        </Layout>
-
-    </div>
-  )
+  );
 }
+ 
+export default Home;
